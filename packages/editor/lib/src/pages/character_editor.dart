87edaced9c612358a88d63b39ad89c4dca90fcdd
邵燃 <chengfubeiming@gmail.com>
2021-12-17 18:11:42 +0800

    2021/12/17b

--- packages/editor/lib/src/pages/character_editor.dart
+++ packages/editor/lib/src/pages/character_editor.dart
@@ -41,37 +41,400 @@ class _CharacterEditorState extends State<CharacterEditor> {
         icon: const Icon(Icons.add),
         backgroundColor: Colors.blueGrey,
       ),
-      body: Container(
-        padding: const EdgeInsets.all(15.0),
-        width: 400,
-        child: Column(
-          children: [
-            TextFormField(
-              controller: _nameTextFieldController,
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: AppLocalizations.of(context)!.characterId,
-              ),
-            ),
-            TextFormField(
-              controller: _nameTextFieldController,
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: AppLocalizations.of(context)!.characterName,
-              ),
-            ),
-            TextFormField(
-              controller: _nameTextFieldController,
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: AppLocalizations.of(context)!.characterAvatar,
-              ),
-            ),
-            TextFormField(
-              controller: _nameTextFieldController,
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: AppLocalizations.of(context)!.characterOrganization,
+      body: Scrollbar(
+        child: ListView(
+          physics: const AlwaysScrollableScrollPhysics(
+              parent: BouncingScrollPhysics()),
+          shrinkWrap: true,
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Wrap(
+                  spacing: 10.0, // gap between adjacent chips
+                  runSpacing: 5.0, // gap between lines
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText:
+                                AppLocalizations.of(context)!.characterId,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText:
+                                AppLocalizations.of(context)!.characterName,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText:
+                                AppLocalizations.of(context)!.characterAvatar,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterOrganization,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterRankInOrganization,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterSuperiorInOrganization,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterLoyaltyInOrganization,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterAllegianceTo,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterAllegiance,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText:
+                                AppLocalizations.of(context)!.characterFame,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText:
+                                AppLocalizations.of(context)!.characterInfamy,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText:
+                                AppLocalizations.of(context)!.characterLooks,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterCurrentLife,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText:
+                                AppLocalizations.of(context)!.characterSpirit,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterCurrentSpirit,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText:
+                                AppLocalizations.of(context)!.characterStamina,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterCurrentStamina,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText:
+                                AppLocalizations.of(context)!.characterStrength,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterDexterity,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterPerception,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterIntelligence,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText:
+                                AppLocalizations.of(context)!.characterMemory,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterWaterSpiritRoot,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterWoodSpiritRoot,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterEarthSpiritRoot,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterMetalSpiritRoot,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: SizedBox(
+                        width: 400,
+                        child: TextFormField(
+                          controller: _nameTextFieldController,
+                          decoration: InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!
+                                .characterFireSpiritRoot,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
