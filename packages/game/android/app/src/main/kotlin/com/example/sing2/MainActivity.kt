6d4b2f143767ff9d9004dc86d876ce4391910b8e
邵燃 <chengfubeiming@gmail.com>
2022-01-13 17:31:47 +0800

    update project name

--- packages/game/android/app/src/main/kotlin/com/example/sing2/MainActivity.kt
+++ packages/game/android/app/src/main/kotlin/com/example/sing2/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.sing2
+package hetu.game.heavenly_tribulation
 
 import io.flutter.embedding.android.FlutterActivity
 
--- packages/game/lib/main.dart
+++ packages/game/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flame/flame.dart';
 
 import 'ui/game_app.dart';
 import 'engine/game.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Flame.device.setPortraitDownOnly();
+  await Flame.device.fullScreen();
+
   runApp(MaterialApp(
       title: 'Tian Dao Qi Jie',
       debugShowCheckedModeBanner: false,
