85e41175d4817d3f43db2ae90b1b7f51b30818ff
hythloday@live.com <hythloday@live.com>
2022-01-25 09:12:54 +0800

    2022/01/25

--- tools/bin/localization_data_conversion.dart
+++
@@ -1,5 +0,0 @@
-import 'package:localization_data_conversion/localization_data_conversion.dart' as localization_data_conversion;
-
-void main(List<String> arguments) {
-  print('Hello world: ${localization_data_conversion.calculate()}!');
-}
