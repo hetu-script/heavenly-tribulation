46e273fe7765668026ead3f5092d16355693c57b
邵燃 <chengfubeiming@gmail.com>
2022-06-16 18:34:40 +0800

    refactor character view ui

---
+++ packages/game/lib/ui/view/character/attributes.dart
@@ -0,0 +1,117 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:hetu_script/values.dart';
+
+import '../../../global.dart';
+import '../../shared/avatar.dart';
+import '../../shared/constants.dart';
+
+class CharacterAttributesView extends StatelessWidget {
+  const CharacterAttributesView({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  final HTStruct data;
+
+  @override
+  Widget build(BuildContext context) {
+    final attributes = data['attributes'];
+    final int strength = attributes['strength'];
+    final int intelligence = attributes['intelligence'];
+    final int perception = attributes['perception'];
+    final int superpower = attributes['superpower'];
+    final int leadership = attributes['leadership'];
+    final int management = attributes['management'];
+
+    final personality = data['personality'];
+    final double ideal = personality['ideal'];
+    final String idealString = ideal.toStringAsFixed(2);
+    final double order = personality['order'];
+    final String orderString = order.toStringAsFixed(2);
+    final double good = personality['good'];
+    final String goodString = good.toStringAsFixed(2);
+    final double social = personality['social'];
+    final String socialString = social.toStringAsFixed(2);
+    final double intuition = personality['intuition'];
+    final String intuitionString = intuition.toStringAsFixed(2);
+    final double reason = personality['reason'];
+    final String reasonString = reason.toStringAsFixed(2);
+    final double controlment = personality['controlment'];
+    final String controlmentString = controlment.toStringAsFixed(2);
+
+    final ageString = engine.hetu.interpreter
+        .invoke('getCharAgeString', positionalArgs: [data]);
+
+    return Container(
+      padding: const EdgeInsets.all(10),
+      height: MediaQuery.of(context).size.height - kTabBarHeight,
+      child: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(
+          dragDevices: {
+            PointerDeviceKind.touch,
+            PointerDeviceKind.mouse,
+          },
+        ),
+        child: ListView(
+          physics: const AlwaysScrollableScrollPhysics(
+            parent: BouncingScrollPhysics(),
+          ),
+          shrinkWrap: true,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.topLeft,
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  child: Avatar(
+                    avatarAssetKey: 'assets/images/${data['avatar']}',
+                  ),
+                ),
+                Text(data['name']),
+                Text(
+                    '${engine.locale['sex']}: ${data['isFemale'] ? engine.locale['female'] : engine.locale['male']}'),
+                Text('${engine.locale['age']}: $ageString'),
+                Text(
+                    '${engine.locale['looks']}: ${data['looks'].toStringAsFixed(2)}'),
+                Text('${engine.locale['fame']}: ${data['fame'].toString()}'),
+                Text('${engine.locale['fame']}: ${data['fame'].toString()}'),
+                Text('---${engine.locale['attributes']}---'),
+                Text('${engine.locale['strength']}: $strength'),
+                Text('${engine.locale['intelligence']}: $intelligence'),
+                Text('${engine.locale['perception']}: $perception'),
+                Text('${engine.locale['superpower']}: $superpower'),
+                Text('${engine.locale['leadership']}: $leadership'),
+                Text('${engine.locale['management']}: $management'),
+                Text('---${engine.locale['personality']}---'),
+                Text(ideal > 0
+                    ? '${engine.locale['ideal']}: +$idealString'
+                    : '${engine.locale['real']}: $idealString'),
+                Text(order > 0
+                    ? '${engine.locale['order']}: +$orderString'
+                    : '${engine.locale['chaotic']}: $orderString'),
+                Text(good > 0
+                    ? '${engine.locale['good']}: +$goodString'
+                    : '${engine.locale['evil']}: $goodString'),
+                Text(social > 0
+                    ? '${engine.locale['social']}: +$socialString'
+                    : '${engine.locale['introspection']}: $socialString'),
+                Text(intuition > 0
+                    ? '${engine.locale['intuition']}: +$intuitionString'
+                    : '${engine.locale['sensing']}: $intuitionString'),
+                Text(reason > 0
+                    ? '${engine.locale['reason']}: +$reasonString'
+                    : '${engine.locale['fealing']}: $reasonString'),
+                Text(controlment > 0
+                    ? '${engine.locale['controlment']}: +$controlmentString'
+                    : '${engine.locale['relaxation']}: $controlmentString'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- packages/game/lib/ui/view/character/character.dart
+++ packages/game/lib/ui/view/character/character.dart
@@ -1,15 +1,13 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 import '../../../global.dart';
-import '../../shared/avatar.dart';
 import 'bonds.dart';
 import '../history.dart';
 import '../../shared/constants.dart';
 import 'skills.dart';
 import '../../shared/responsive_route.dart';
 import '../../shared/close_button.dart';
+import 'attributes.dart';
 
 class CharacterView extends StatelessWidget {
   final String? characterId;
@@ -23,26 +21,6 @@ class CharacterView extends StatelessWidget {
 
     final data = engine.hetu.interpreter
         .invoke('getCharacterById', positionalArgs: [charId]);
-
-    final attributes = data['attributes'];
-    final int strength = attributes['strength'];
-    final int intelligence = attributes['intelligence'];
-    final int perception = attributes['perception'];
-    final int superpower = attributes['superpower'];
-    final int leadership = attributes['leadership'];
-    final int management = attributes['management'];
-
-    final personality = data['personality'];
-    final double ideal = personality['ideal'].toStringAsFixed(2);
-    final double order = personality['order'].toStringAsFixed(2);
-    final double good = personality['good'].toStringAsFixed(2);
-    final double social = personality['social'].toStringAsFixed(2);
-    final double intuition = personality['intuition'].toStringAsFixed(2);
-    final double reason = personality['reason'].toStringAsFixed(2);
-    final double controlment = personality['controlment'].toStringAsFixed(2);
-
-    final ageString = engine.hetu.interpreter
-        .invoke('getCharAgeString', positionalArgs: [data]);
 
     final layout = DefaultTabController(
       length: kCharacterViewTabLengths,
@@ -74,133 +52,7 @@ class CharacterView extends StatelessWidget {
         ),
         body: TabBarView(
           children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(10),
-              height: MediaQuery.of(context).size.height - kTabBarHeight,
-              child: SingleChildScrollView(
-                child: ScrollConfiguration(
-                  behavior: ScrollConfiguration.of(context).copyWith(
-                    dragDevices: {
-                      PointerDeviceKind.touch,
-                      PointerDeviceKind.mouse,
-                    },
-                  ),
-                  child: ListView(
-                    physics: const AlwaysScrollableScrollPhysics(
-                        parent: BouncingScrollPhysics()),
-                    shrinkWrap: true,
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Container(
-                            alignment: Alignment.center,
-                            padding: const EdgeInsets.symmetric(vertical: 40),
-                            child: Avatar(
-                              avatarAssetKey: 'assets/images/${data['avatar']}',
-                            ),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(data['name']),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(
-                                '${engine.locale['sex']}: ${data['isFemale'] ? engine.locale['female'] : engine.locale['male']}'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text('${engine.locale['age']}: $ageString'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(
-                                '${engine.locale['looks']}: ${data['looks'].toStringAsFixed(2)}'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(
-                                '${engine.locale['fame']}: ${data['fame'].toString()}'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child:
-                                Text('${engine.locale['strength']}: $strength'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(
-                                '${engine.locale['intelligence']}: $intelligence'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(
-                                '${engine.locale['perception']}: $perception'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(
-                                '${engine.locale['superpower']}: $superpower'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(
-                                '${engine.locale['leadership']}: $leadership'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(
-                                '${engine.locale['management']}: $management'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(ideal > 0
-                                ? '${engine.locale['ideal']}(+$ideal)'
-                                : '${engine.locale['real']}($ideal)'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(order > 0
-                                ? '${engine.locale['order']}(+$order)'
-                                : '${engine.locale['chaotic']}($order)'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(good > 0
-                                ? '${engine.locale['good']}(+$good)'
-                                : '${engine.locale['evil']}($good)'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(social > 0
-                                ? '${engine.locale['social']}(+$social)'
-                                : '${engine.locale['introspection']}($social)'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(intuition > 0
-                                ? '${engine.locale['intuition']}(+$intuition)'
-                                : '${engine.locale['sensing']}($intuition)'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(reason > 0
-                                ? '${engine.locale['reason']}(+$reason)'
-                                : '${engine.locale['fealing']}($reason)'),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(5),
-                            child: Text(controlment > 0
-                                ? '${engine.locale['controlment']}(+$controlment)'
-                                : '${engine.locale['relaxation']}($controlment)'),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            CharacterAttributesView(data: data),
             CharacterSkillsView(data: data['skills']),
             CharacterBondsView(data: data['bonds']),
             HistoryView(data: data['experiencedIncidentIndexes']),
